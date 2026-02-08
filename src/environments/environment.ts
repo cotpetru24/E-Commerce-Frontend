6eed091862fae3717c291467c2f9faaecebfac2e
cotpetru24 <cotpetru1@gmail.com>
2026-02-08 19:44:15 +0000

    fixing environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'https://localhost:5001',
+  stripePublishableKey:'pk_test_51Ply1TBlRu5FugfukzYTsihdCL7q6Kasdq8sDh9VcldWFTnh3c07OY1EMf7e3qqVon1YkIlZjMnNMt426yZBaS4f00GflfIjk1'
+};
